f7ad7a494d07c35e378da30e69f29826c900a06e
阿男 <l.weinan@gmail.com>
2023-12-31 00:23:51 +0800

    add jberet

---
+++ src/main/java/SimpleChunkItemReader.java
@@ -0,0 +1,10 @@
+import jakarta.batch.api.chunk.AbstractItemReader;
+
+//@Named
+public class SimpleChunkItemReader extends AbstractItemReader {
+
+    @Override
+    public Object readItem() throws Exception {
+        return null;
+    }
+}
